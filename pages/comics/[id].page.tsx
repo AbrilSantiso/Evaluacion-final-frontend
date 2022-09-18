0d3681884ff7f37aa2bb8ee1bb2a9efbf143ecdb
Abril Santiso Cabelluzi <abril.santiso@globant.com>
2022-09-17 22:19:10 -0300

    finished comic page

--- pages/comics/[id].page.tsx
+++ pages/comics/[id].page.tsx
@@ -8,23 +8,33 @@ import {
   CardActions,
   Box,
   Grid,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
 } from "@mui/material";
 import { ProductCardProps as ComicPageProps } from "types";
 import Image from "next/image";
 import { getComic } from "dh-marvel/services/marvel/marvel.service";
 import { GetServerSideProps, NextPage } from "next/types";
+import { ExpandMore } from "@mui/icons-material";
+import Link from "next/link";
 
 const ComicPage: NextPage<ComicPageProps> = ({ comic }) => {
   const image = `${comic.thumbnail.path}.${comic.thumbnail.extension}`;
+  if (comic) {
+    console.log(comic);
+  }
   return (
-    <Box sx={{
+    <Box
+      sx={{
         display: "flex",
-       flexDirection: "column",
+        flexDirection: "column",
         alignItems: "center",
         width: "100%",
-      }}>
+      }}
+    >
       <Typography
-      component="h2"
+        component="h2"
         sx={{
           fontSize: "28px",
           fontWeight: "600",
@@ -43,6 +53,7 @@ const ComicPage: NextPage<ComicPageProps> = ({ comic }) => {
           flexFlow: "row wrap",
           marginTop: "40px",
           alignItems: "center",
+          justifyContent: "center",
           marginBottom: "32px",
           maxWidth: "700px",
         }}
@@ -54,38 +65,96 @@ const ComicPage: NextPage<ComicPageProps> = ({ comic }) => {
             width: "300px",
             height: "350px",
             objectFit: "contain",
-            
           }}
         />
-        <Card sx={{ width: "300px", padding:"16px" }}>
-          
+        <Card sx={{ width: "300px", padding: "16px" }}>
+          <Typography
+            sx={{ fontSize: "1.5rem", width: "80%" }}
+            gutterBottom
+            variant="h4"
+          >
+            {comic.title}
+          </Typography>
+          {comic.stock > 0 && (
             <Typography
-              sx={{ height: "80px", fontSize: "1.5rem", width:"80%" }}
-              gutterBottom
-              variant="h4"
-            >
-              {comic.title}
-            </Typography>
-            { comic.stock > 0 && <Typography
-              sx={{ fontSize: "14px", color:"rgba(0, 0, 0, 0.6)"}}
-              gutterBottom
-              variant="h6"
-            > {`Antes $${comic.oldPrice}`}
-                </Typography>}
-            <Typography
-              sx={{ fontSize: "18px" }}
+              sx={{ fontSize: "14px", color: "rgba(0, 0, 0, 0.6)" }}
               gutterBottom
               variant="h6"
             >
-              {`$${comic.price}`}
+              {" "}
+              {`Antes $${comic.oldPrice}`}
             </Typography>
-       
+          )}
+          <Typography sx={{ fontSize: "18px" }} gutterBottom variant="h6">
+            {`$${comic.price}`}
+          </Typography>
+
           <CardActions>
-            <Button color="primary">Comprar</Button>
+            {comic.stock > 0 ? (
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                fullWidth
+              >
+                Comprar
+              </Button>
+            ) : (
+              <Button variant="contained" disabled size="large" fullWidth>
+                Sin stock disponible
+              </Button>
+            )}
           </CardActions>
         </Card>
       </Box>
-      <Box>Aca van descripcion y personajes</Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "100%",
+          marginBottom:"20px"
+        }}
+      >
+        <Accordion
+          sx={{
+            maxWidth: "700px",
+            width: "90%",
+            marginBottom: "20px",
+          }}
+        >
+          <AccordionSummary expandIcon={<ExpandMore />}>
+            Descripción
+          </AccordionSummary>
+          <AccordionDetails>
+            {comic.description
+              ? comic.description
+              : "Sin descripción disponible"}
+          </AccordionDetails>
+        </Accordion>
+        {comic.characters.items.length && (
+          <Accordion
+            sx={{
+              maxWidth: "700px",
+              width: "90%",
+              marginBottom: "20px",
+            }}
+          >
+            <AccordionSummary expandIcon={<ExpandMore />}>
+              Personajes
+            </AccordionSummary>
+            <AccordionDetails>
+                <Grid container spacing={2}>
+             {comic.characters.items.map((item)=>(
+              <Grid key={item.name} item>
+                <Link href={`/personajes/${item.resourceURI.split("/")[6]}`}>{item.name}</Link>
+                </Grid>
+             ))}
+             </Grid>
+            </AccordionDetails>
+          </Accordion>
+        )}
+      </Box>
     </Box>
   );
 };
