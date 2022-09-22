20cdc43b449c846d25de69a71ccca1e8bdc1fe91
AbrilSantiso <abrilsantiso@hotmail.com>
2022-09-22 20:44:17 -0300

    fix forms compilation

---
+++ components/forms/AddressForm.tsx
@@ -0,0 +1,19 @@
+import { useForm } from 'react-hook-form';
+
+import {Box} from "@mui/material";
+import { FC } from 'react';
+
+const AddressForm:FC = () => {
+
+    const {handleSubmit, control} = useForm();
+    const onSubmit = (data:any) => console.log(data);
+
+    return (
+        <Box sx={{width: '100%', display:"flex", flexDirection:"column", alignItems:"center"}}  component="form" onSubmit={handleSubmit(onSubmit)}>
+         
+            
+        </Box>
+    )
+}
+
+export default AddressForm;
